73c562cb2228936808df3f090ba0ca5189980bea
broothie <adbooth8@gmail.com>
2023-07-08 11:59:07 -0700

    fix hashbang

--- bin/update_spec_case_files.sh
+++ bin/update_spec_case_files.sh
@@ -1,9 +1,11 @@
-#! env bash
+#! /usr/bin/env bash
 
+echo "Update valid.yml"
 curl https://api.github.com/repos/jetpack-io/typeid/contents/spec/valid.yml \
   -H 'Accept: application/vnd.github.raw' \
   > spec/valid.yml
 
+echo "Update invalid.yml"
 curl https://api.github.com/repos/jetpack-io/typeid/contents/spec/invalid.yml \
   -H 'Accept: application/vnd.github.raw' \
   > spec/invalid.yml
