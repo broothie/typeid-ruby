75066f677a112d27d2f92002ed18112eaa6b2acc
Andrew Booth <adbooth8@gmail.com>
2024-04-12 17:29:28 +0000

    update spec download script

--- bin/update_spec_case_files.sh
+++ bin/update_spec_case_files.sh
@@ -1,11 +1,7 @@
 #! /usr/bin/env bash
 
 echo "Update valid.yml"
-curl https://api.github.com/repos/jetpack-io/typeid/contents/spec/valid.yml \
-  -H 'Accept: application/vnd.github.raw' \
-  > spec/valid.yml
+curl https://raw.githubusercontent.com/jetify-com/typeid/main/spec/valid.yml > spec/valid.yml
 
 echo "Update invalid.yml"
-curl https://api.github.com/repos/jetpack-io/typeid/contents/spec/invalid.yml \
-  -H 'Accept: application/vnd.github.raw' \
-  > spec/invalid.yml
+curl https://raw.githubusercontent.com/jetify-com/typeid/main/spec/invalid.yml > spec/invalid.yml
